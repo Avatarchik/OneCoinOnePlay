4909d23ab8adafa8ad7606401acc30b90b29a824
장진우 <zzangjjw4406@gmail.com>
2016-04-04 12:41:53 +0900

    # 구글 플레이 계정 테스트 및 UI 수정
    1. 구글 플레이 서비스 테스트
    -> 리더보드 및 업적 보여주기 테스트 완료
    2. 기타 UI 관련 수정

--- Assets/Assets/Script/MainMenu/MainMenuManager.cs
+++ Assets/Assets/Script/MainMenu/MainMenuManager.cs
@@ -7,27 +7,58 @@ using UnityEngine.SocialPlatforms;
 
 public class MainMenuManager : MonoBehaviour {
 
+    private bool isLoginSuccess = false;
+
     void Start()
+    {
+        InitGPG();
+        LogingGPG();
+    }
+
+    public void InitGPG()
     {
         // Activate the Google Play Games platform
         PlayGamesPlatform.Activate();
     }
 
-	public void GameStart()
+    public void LogingGPG()
     {
         Social.localUser.Authenticate((bool success) => {
             // handle success or failure
-            if(success)
+            if (success)
             {
-                SceneManager.LoadSceneAsync("InGame");
+                isLoginSuccess = true;
             }
             else
             {
-                
+                isLoginSuccess = false;
             }
-            
         });
+    }
+
+	public void GameStart()
+    {
+        if(isLoginSuccess)
+        {
+            SceneManager.LoadSceneAsync("InGame");
+        }
+        else
+        {
+            LogingGPG();
+        }
         
+    }
+
+    public void OnClickLeaderBoard()
+    {
+        // show leaderboard UI
+        Social.ShowLeaderboardUI();
+    }
+
+    public void OnClickAchivement()
+    {
+        // show achievements UI
+        Social.ShowAchievementsUI();
     }
 
     public void GameExit()
